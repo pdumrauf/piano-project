2fdf27ab7f237eae3f9ee9acecdbdf4fb14fc0eb
pdumrauf <dumraufp@gmail.com>
2022-11-04 19:11:54 +0100

    all done

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const pianoKeys = document.querySelectorAll('.key')
 
 function playSound(sound) {
-  const click = 'click al pianito'
-  console.log(click)
-
   new Audio(sound).play()
 }
 
 pianoKeys.forEach((key, i) => {
-  const num = i < 9 ? i + (i + 1) : (i + 1)
+  const num = i < 9 ? '0' + (i + 1) : (i + 1)
   const keySound = `piano-keys-audio/key${num}.mp3`
-  
+
   key.addEventListener('click', () => playSound(keySound))
 })
